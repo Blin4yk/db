12b5422c4026dad0e72ebd448f495ad7eb76ba84
Никита Максимович <nshelonin@mail.ru>
2023-07-11 19:31:05 +0300

    Добавил выпадение сокровища, в бд добавил новые поля, в таблице лидеров показывается кол-во алмазов

--- results.py
+++ results.py
@@ -1,10 +1,28 @@
 from connect import connection
+from random import randint as r
 
 class Money:
-    def __init__(self, id, money_value):
-        with connection.cursor() as cursor:
-            cursor.execute(f"UPDATE `gamers` SET money = money + {money_value} WHERE id_user = {id};")
-            connection.commit()
+    def __init__(self, id, money_value, value):
+        if value == 'coin':
+            with connection.cursor() as cursor:
+                cursor.execute(f"UPDATE `gamers` SET money = money + {money_value} WHERE id_user = {id};")
+                connection.commit()
+        elif value == 'diam':
+            with connection.cursor() as cursor:
+                cursor.execute(f"UPDATE `gamers` SET money = money + 10000 WHERE id_user = {id};")
+                connection.commit()
+            with connection.cursor() as cursor:
+                cursor.execute(f"UPDATE `gamers` SET diamonds = diamonds + 1 WHERE id_user = {id};")
+                connection.commit()
+
+        elif value == 'treasure':
+            with connection.cursor() as cursor:
+                cursor.execute(f"UPDATE `gamers` SET money = money + {r(0, 100000)} WHERE id_user = {id};")
+                connection.commit()
+
+            with connection.cursor() as cursor:
+                cursor.execute(f"UPDATE `gamers` SET treasure = treasure + 1 WHERE id_user = {id};")
+                connection.commit()
 
 # Баланс игрока
 def balance(id):
@@ -17,8 +35,10 @@ def balance(id):
 def leaders(id):
     table_leaders = ""
     with connection.cursor() as cursor:
-        cursor.execute(f"SELECT name, money FROM `gamers` ORDER BY money DESC")
+        cursor.execute(f"SELECT name, money, diamonds FROM `gamers` ORDER BY money DESC")
         money_value = cursor.fetchmany(5)
         for row in money_value:
-            table_leaders += f"{money_value.index(row)+1}. {row['name']} --> Золото: {row['money']}\n"
+            table_leaders += f"{money_value.index(row)+1}. {row['name']} " \
+                             f"--> Золото: {row['money']} " \
+                             f"| Алмазов: {row['diamonds']}\n"
     return table_leaders
--- telegram.py
+++ telegram.py
@@ -1,4 +1,7 @@
 import logging
+
+import aiogram.dispatcher.filters.state
+
 import connect
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types import ReplyKeyboardRemove, \
@@ -7,8 +10,8 @@ from aiogram.types import ReplyKeyboardRemove, \
 import results
 from random import randint as r
 from time import sleep as t
-
-API = "6312267659:AAHDbFXldMS3_Q2Wo3a4Ao3JusqTF62LyZs" # токен бота
+from aiogram.dispatcher.filters.state import State, StatesGroup
+API = "6312267659:AAHDbFXldMS3_Q2Wo3a4Ao3JusqTF62LyZs"  # токен бота
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -18,19 +21,24 @@ bot = Bot(token=API)
 dp = Dispatcher(bot)
 
 # Кнопки
-buttons = ["Испытать удачу","Магазин","Тап ₽"]
+buttons = ["Испытать удачу", "Магазин", "Тап ₽"]
 kb = ReplyKeyboardMarkup(resize_keyboard=True).add(*buttons)
 
 # CallBack кнопки
-callback_kb = types.InlineKeyboardMarkup()
-callback_kb.add(types.InlineKeyboardButton(text="Лидеры", callback_data="leaders"))
+callback_kb = InlineKeyboardMarkup()
+callback_kb.add(InlineKeyboardButton(text="Лидеры", callback_data="leaders"))
+# callback_kb.add(types.InlineKeyboardButton(text="Испытать удачу", callback_data="lucky_game"))
+
+callback_lucky = InlineKeyboardMarkup()
+callback_lucky.add(InlineKeyboardButton(text="Испытать удачу", callback_data="lucky_game"))
 
 # Приветствие
 @dp.message_handler(commands=['start'])
 async def start(msg: types.Message):
     try:
         with connect.connection.cursor() as cursor:
-            cursor.execute(f"INSERT INTO `gamers`(name, premium, username, id_user) VALUES ('{msg.from_user.first_name}',"
+            cursor.execute(f"INSERT INTO `gamers`(name, premium, username, id_user) VALUES "
+                           f"('{msg.from_user.first_name}',"
                            f"'{msg.from_user.is_premium}',"
                            f"'{msg.from_user.username}',"
                            f"{msg.from_user.id})")
@@ -42,25 +50,47 @@ async def start(msg: types.Message):
         connect.connection.cursor().close()
     await msg.answer('Привет, это игра монетка, чем больше тапаешь, тем больше монеток зарабатываешь', reply_markup=kb)
 
+# Создаем класс состояний
+class States(StatesGroup):
+    waiting_for_message = State()
 
 @dp.message_handler()
 async def tap(msg: types.Message):
     if msg.text == "Тап ₽":
         value = r(0,1001)
         unlucky_value = open('img/photo_2023-07-10_19-54-11.jpg', 'rb')
-        lucky_value = open('C:\pythonProject2\img\pixil-frame-0 (4).png', 'rb')
+        lucky_value = open('img/pixil-frame-0 (4).png', 'rb')
+        treasure = open('img/pixil-frame-0 (5).png', 'rb')
 
-        if value < 1000:
-            results.Money(msg.from_user.id, 1)
-            await bot.send_photo(msg.chat.id, unlucky_value, caption=f'В сундуке {results.balance(msg.from_user.id)} монет', reply_markup=callback_kb)
-
+        if value < 980 and value != 777:
+            results.Money(msg.from_user.id, 1, "coin")
+            await bot.send_photo(msg.chat.id, unlucky_value,
+                                 caption=f'В сундуке {results.balance(msg.from_user.id)} монет',
+                                 reply_markup=callback_kb)
+        elif value == 777:
+            results.Money(msg.from_user.id, 1, "treasure")
+            await bot.send_photo(msg.chat.id, treasure,
+                                 caption=f'В сундуке {results.balance(msg.from_user.id)} монет',
+                                 reply_markup=callback_kb)
         else:
-            results.Money(msg.from_user.id, value * 10)
-            await bot.send_photo(msg.chat.id, lucky_value, caption=f'О повезло повезло начислено {value*10}, в сундуке {results.balance(msg.from_user.id)} монет', reply_markup=callback_kb)
+            results.Money(msg.from_user.id, value * 10, "diam")
+            await bot.send_photo(msg.chat.id, lucky_value,
+                                 caption=f'О повезло повезло начислено {10000}, в сундуке '
+                                         f'{results.balance(msg.from_user.id)} монет',
+                                 reply_markup=callback_kb)
+
+    elif msg.text == "Испытать удачу":
+        await msg.answer(f"Баланс {results.balance(msg.from_user.id)}", reply_markup=callback_lucky)
 
 @dp.callback_query_handler(text="leaders")
 async def leader(cbq: CallbackQuery):
     await bot.answer_callback_query(cbq.id, text=f"{results.leaders(cbq.from_user.id)}", show_alert=True)
+
+@dp.callback_query_handler(text="lucky_game")
+async def leader(cbq: CallbackQuery):
+    await cbq.answer("Какую сумму вы хотите поставить?")
+
+
 
 
 if __name__ == '__main__':
